2f1f0cc2f8b86b7012fb6191410a7fb7b5fb78d7
Jono Brandel <jonobrandel@google.com>
2013-01-01 17:16:47 -0800

    moved license to root of project.

--- utils/build.js
+++ utils/build.js
@@ -3,7 +3,7 @@
 var compressor = require('node-minify');
 
 var files = [
-  '../src/license.txt',
+  '../license.txt',
   '../third-party/underscore.js',
   '../third-party/Three.js',
   '../src/two.js'
