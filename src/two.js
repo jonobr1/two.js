25dfddea0604082e562f4424c7e8cf94e6da3b30
Jono Brandel <jonobrandel@google.com>
2013-07-29 20:09:56 -0700

    cleaned up white spaces

--- src/two.js
+++ src/two.js
@@ -1266,14 +1266,12 @@
 
   })();
 
-
   //exports to multiple environments
   if (typeof define === 'function' && define.amd)
-	//AMD
-	define(function(){ return Two; });
+  //AMD
+  define(function(){ return Two; });
   else if (typeof module != "undefined" && module.exports)
-	//Node
-	module.exports = Two;
-
+  //Node
+  module.exports = Two;
 
 })();
