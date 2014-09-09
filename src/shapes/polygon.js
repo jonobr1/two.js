2ba5c70e7cc41072ef960f8ec7cabf9b92fbca87
jonobr1 <jonobrandel@gmail.com>
2014-09-09 15:13:19 -0700

    Two.Polygon in the illustrator sense

--- src/shapes/polygon.js
+++ src/shapes/polygon.js
@@ -1,10 +1,21 @@
 (function() {
 
-  var Path = Two.Path;
+  var Path = Two.Path, TWO_PI = Math.PI * 2, cos = Math.cos, sin = Math.sin;
 
-  var Polygon = Two.Polygon = function(points) {
+  var Polygon = Two.Polygon = function(ox, oy, r, sides) {
+
+    sides = Math.max(sides || 0, 3);
+
+    var points = _.map(_.range(sides), function(i) {
+      var pct = (i + 0.5) / sides;
+      var theta = TWO_PI * pct + Math.PI / 2;
+      var x = r * cos(theta);
+      var y = r * sin(theta);
+      return new Two.Anchor(x, y);
+    });
 
     Path.call(this, points, true);
+    this.translation.set(ox, oy);
 
   };
 
