f7760f72e4c4f4a18d9017f9f1241706f421d6f8
jonobr1 <jonobrandel@gmail.com>
2015-10-07 16:41:34 -0700

    updated two.js removed Two.CurvedPolygon

--- third-party/two.js
+++ third-party/two.js
@@ -3178,9 +3178,9 @@ var Backbone = Backbone || {};
 
     },
 
-    makeRoundedRectangle: function(x, y, width, height, radius) {
+    makeRoundedRectangle: function(x, y, width, height, sides) {
 
-      var rect = new Two.RoundedRectangle(x, y, width, height, radius);
+      var rect = new Two.RoundedRectangle(x, y, width, height, sides);
       this.scene.add(rect);
 
       return rect;
@@ -3244,19 +3244,6 @@ var Backbone = Backbone || {};
       this.scene.add(poly);
 
       return poly;
-
-    },
-
-    /*
-    * Make a Curved Polygon in scene
-    */
-
-    makeCurvedPolygon: function(ox, oy, r, sides, mod) {
-
-      var curvedPoly = new Two.CurvedPolygon(ox, oy, r, sides, mod);
-      this.scene.add(curvedPoly);
-
-      return curvedPoly;
 
     },
 
@@ -7220,85 +7207,6 @@ var Backbone = Backbone || {};
   _.extend(Polygon.prototype, Path.prototype);
 
   Path.MakeObservable(Polygon.prototype);
-
-})(
-  Two,
-  typeof require === 'function' ? require('underscore') : _,
-  typeof require === 'function' ? require('backbone') : Backbone,
-  typeof require === 'function' ? require('requestAnimationFrame') : requestAnimationFrame
-);
-
-(function(Two, _, Backbone, requestAnimationFrame) {
-
-  var Path = Two.Path, PI = Math.PI, TWO_PI = Math.PI * 2, cos = Math.cos, sin = Math.sin, abs = Math.abs;
-
-  var CurvedPolygon = Two.CurvedPolygon = function(ox, oy, r, sides, mod) {
-
-    var points = [];
-    sides = Math.max(sides || 0, 3);
-    var sidesBump = sides+1;
-    var angleStep = TWO_PI / sides;
-    var bezierDelta =  PI * r / sides / 2 ;
-    mod = mod || 0;
-
-    var command = Two.Commands.move;
-    var theta = PI, x, y, lx, ly, rx, ry;
-
-    points.push(
-      new Two.Anchor(
-        sin(theta) * (r),
-        cos(theta) * (r),
-        0,0,0,0,
-        command
-      )
-    );
-
-    for (var i = 0; i < sidesBump; i++) {
-
-      theta = (angleStep * i) + PI;
-      command = Two.Commands.curve;
-
-      x = Math.sin(theta) * r;
-      y = Math.cos(theta) * r;
-
-      if (mod >= 0) {
-
-        lx = (sin(theta - (Math.PI/2))) * bezierDelta * mod;
-        ly = (cos(theta - (Math.PI/2))) * bezierDelta * mod;
-        rx = (sin(theta + (Math.PI/2))) * bezierDelta * mod;
-        ry = (cos(theta + (Math.PI/2))) * bezierDelta * mod;
-
-      } else {
-
-        lx = (sin(theta - (Math.PI))) * bezierDelta * abs(mod);
-        ly = (cos(theta - (Math.PI))) * bezierDelta * abs(mod);
-        rx = (sin(theta + (Math.PI))) * bezierDelta * abs(mod);
-        ry = (cos(theta + (Math.PI))) * bezierDelta * abs(mod);
-
-      }
-
-      if (i === 0) {
-        lx = 0;
-        ly = 0;
-      }
-
-      if (i === sidesBump - 1) {
-        rx = 0;
-        ry = 0;
-      }
-
-      points.push(new Two.Anchor(x, y, lx, ly, rx, ry, command));
-
-    }
-
-    Path.call(this, points, true, false, true);
-    this.translation.set(ox, oy);
-
-  };
-
-  _.extend(CurvedPolygon.prototype, Path.prototype);
-
-  Path.MakeObservable(CurvedPolygon.prototype);
 
 })(
   Two,
