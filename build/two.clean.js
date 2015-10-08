54f374660d94d715623c36e9f0173825f03e1be7
jonobr1 <jonobrandel@gmail.com>
2015-10-07 23:07:37 -0700

    updated Stop to be namespaced to Two, ie Two.Stop

--- build/two.clean.js
+++ build/two.clean.js
@@ -6101,7 +6101,7 @@
 
 (function(Two, _, Backbone, requestAnimationFrame) {
 
-  var Stop = function(offset, color, opacity) {
+  var Stop = Two.Stop = function(offset, color, opacity) {
 
     this._renderer = {};
 
