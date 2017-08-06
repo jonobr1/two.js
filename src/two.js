c9b1ce734bf54f02bd40918da6c75b2fea500928
Joseph Orbegoso Pea <trusktr@gmail.com>
2017-08-06 10:54:28 -0700

    fix global detection to work in strict mode

--- src/two.js
+++ src/two.js
@@ -1,6 +1,6 @@
 this.Two = (function(previousTwo) {
 
-  var root = this;
+  var root = typeof window != 'undefined' ? window : typeof global != 'undefined' ? global : null;
   var _ = {
     // http://underscorejs.org/ • 1.8.3
     _indexAmount: 0,
