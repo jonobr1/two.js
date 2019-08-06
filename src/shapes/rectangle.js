b5407499218fd2dd4753e1e7ac52490459cb623b
Jono <developer@jono.fyi>
2019-08-06 18:35:29 +0200

    Rectangle Documentation

--- src/shapes/rectangle.js
+++ src/shapes/rectangle.js
@@ -3,6 +3,15 @@
   var Path = Two.Path;
   var _ = Two.Utils;
 
+  /**
+   * @name Two.Rectangle
+   * @class
+   * @extends Two.Path
+   * @param {Number} [x=0] - The x position of the rectangle.
+   * @param {Number} [y=0] - The y position of the rectangle.
+   * @param {Number} width - The width value of the rectangle.
+   * @param {Number} height - The width value of the rectangle.
+   */
   var Rectangle = Two.Rectangle = function(x, y, width, height) {
 
     Path.call(this, [
@@ -13,9 +22,21 @@
       // new Two.Anchor() // TODO: Figure out how to handle this for `beginning` / `ending` animations
     ], true, false, true);
 
+    /**
+     * @name Two.Rectangle#width
+     * @property {Number} - The size of the width of the rectangle.
+     */
     this.width = width;
+    /**
+     * @name Two.Rectangle#height
+     * @property {Number} - The size of the height of the rectangle.
+     */
     this.height = height;
 
+    /**
+     * @name Two.Rectangle#origin
+     * @property {Number} - A two-component vector describing the origin offset to draw the rectangle. Default is `0, 0`.
+     */
     this.origin = new Two.Vector();
     this.translation.set(x, y);
 
@@ -25,8 +46,18 @@
 
   _.extend(Rectangle, {
 
+    /**
+     * @name Two.Rectangle.Properties
+     * @property {String[]} - A list of properties that are on every {@link Two.Rectangle}.
+     */
     Properties: ['width', 'height'],
 
+    /**
+     * @name Two.Rectangle.MakeObservable
+     * @function
+     * @param {Object} object - The object to make observable.
+     * @description Convenience function to apply observable qualities of a {@link Two.Rectangle} to any object. Handy if you'd like to extend the {@link Two.Rectangle} class on a custom class.
+     */
     MakeObservable: function(object) {
 
       Path.MakeObservable(object);
@@ -53,16 +84,44 @@
 
   _.extend(Rectangle.prototype, Path.prototype, {
 
-    _width: 0,
-    _height: 0,
-
+    /**
+     * @name Two.Rectangle#_flagWidth
+     * @private
+     * @property {Boolean} - Determines whether the {@link Two.Rectangle#width} needs updating.
+     */
     _flagWidth: 0,
+    /**
+     * @name Two.Rectangle#_flagHeight
+     * @private
+     * @property {Boolean} - Determines whether the {@link Two.Rectangle#height} needs updating.
+     */
     _flagHeight: 0,
+
+    /**
+     * @name Two.Rectangle#_width
+     * @private
+     * @see {@link Two.Rectangle#width}
+     */
+    _width: 0,
+    /**
+     * @name Two.Rectangle#_height
+     * @private
+     * @see {@link Two.Rectangle#height}
+     */
+    _height: 0,
 
     _origin: null,
 
     constructor: Rectangle,
 
+    /**
+     * @name Two.Rectangle#_update
+     * @function
+     * @private
+     * @param {Boolean} [bubbles=false] - Force the parent to `_update` as well.
+     * @description This is called before rendering happens by the renderer. This applies all changes necessary so that rendering is up-to-date but not updated more than it needs to be.
+     * @nota-bene Try not to call this method more than once a frame.
+     */
     _update: function() {
 
       if (this._flagWidth || this._flagHeight) {
@@ -87,6 +146,12 @@
 
     },
 
+    /**
+     * @name Two.Rectangle#flagReset
+     * @function
+     * @private
+     * @description Called internally to reset all flags. Ensures that only properties that change are updated before being sent to the renderer.
+     */
     flagReset: function() {
 
       this._flagWidth = this._flagHeight = false;
@@ -96,6 +161,13 @@
 
     },
 
+    /**
+     * @name Two.Rectangle#clone
+     * @function
+     * @param {Two.Group} [parent] - The parent group or scene to add the clone to.
+     * @returns {Two.Rectangle}
+     * @description Create a new instance of {@link Two.Rectangle} with the same properties of the current path.
+     */
     clone: function(parent) {
 
       var clone = new Rectangle(0, 0, this.width, this.height);
