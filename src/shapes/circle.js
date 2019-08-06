5f8b3169e84f822414f31dadf431696336f9d096
Jono <developer@jono.fyi>
2019-08-06 18:15:23 +0200

    Circle Documentation

--- src/shapes/circle.js
+++ src/shapes/circle.js
@@ -7,6 +7,15 @@
   // Circular coefficient
   var c = (4 / 3) * Math.tan(Math.PI / 8);
 
+  /**
+   * @name Two.Circle
+   * @class
+   * @extends Two.Path
+   * @param {Number} [x=0] - The x position of the circle.
+   * @param {Number} [y=0] - The y position of the circle.
+   * @param {Number} radius - The radius value of the circle.
+   * @param {Number} [resolution=12] - The number of vertices used to construct the circle.
+   */
   var Circle = Two.Circle = function(ox, oy, r, res) {
 
     var amount = res || 5;
@@ -17,17 +26,37 @@
 
     Path.call(this, points, true, true, true);
 
+    /**
+     * @name Two.Circle#radius
+     * @property {Number} - The size of the radius of the circle.
+     */
     this.radius = r;
 
     this._update();
-    this.translation.set(ox, oy);
+
+    if (_.isNumber(ox)) {
+      this.translation.x = ox;
+    }
+    if (_.isNumber(oy)) {
+      this.translation.y = oy;
+    }
 
   };
 
   _.extend(Circle, {
 
+    /**
+     * @name Two.Circle.Properties
+     * @property {String[]} - A list of properties that are on every {@link Two.Circle}.
+     */
     Properties: ['radius'],
 
+    /**
+     * @name Two.Circle.MakeObservable
+     * @function
+     * @param {Object} object - The object to make observable.
+     * @description Convenience function to apply observable qualities of a {@link Two.Circle} to any object. Handy if you'd like to extend the {@link Two.Circle} class on a custom class.
+     */
     MakeObservable: function(obj) {
 
       Path.MakeObservable(obj);
@@ -39,11 +68,30 @@
 
   _.extend(Circle.prototype, Path.prototype, {
 
-    _radius: 0,
+    /**
+     * @name Two.Circle#_flagRadius
+     * @private
+     * @property {Boolean} - Determines whether the {@link Two.Circle#radius} needs updating.
+     */
     _flagRadius: false,
+
+    /**
+     * @name Two.Circle#_radius
+     * @private
+     * @see {@link Two.Circle#radius}
+     */
+    _radius: 0,
 
     constructor: Circle,
 
+    /**
+     * @name Two.Circle#_update
+     * @function
+     * @private
+     * @param {Boolean} [bubbles=false] - Force the parent to `_update` as well.
+     * @description This is called before rendering happens by the renderer. This applies all changes necessary so that rendering is up-to-date but not updated more than it needs to be.
+     * @nota-bene Try not to call this method more than once a frame.
+     */
     _update: function() {
 
       if (this._flagRadius) {
@@ -81,6 +129,12 @@
 
     },
 
+    /**
+     * @name Two.Circle#flagReset
+     * @function
+     * @private
+     * @description Called internally to reset all flags. Ensures that only properties that change are updated before being sent to the renderer.
+     */
     flagReset: function() {
 
       this._flagRadius = false;
@@ -90,6 +144,13 @@
 
     },
 
+    /**
+     * @name Two.Circle#clone
+     * @function
+     * @param {Two.Group} [parent] - The parent group or scene to add the clone to.
+     * @returns {Two.Circle}
+     * @description Create a new instance of {@link Two.Circle} with the same properties of the current path.
+     */
     clone: function(parent) {
 
       var clone = new Circle(0, 0, this.radius, this.vertices.length);
