d7d4875239d8dece247747a0065fed87861943b8
Jono <developer@jono.fyi>
2019-08-06 18:21:48 +0200

    Ellipse Documentation

--- src/shapes/ellipse.js
+++ src/shapes/ellipse.js
@@ -7,6 +7,16 @@
   // Circular coefficient
   var c = (4 / 3) * Math.tan(Math.PI / 8);
 
+  /**
+   * @name Two.Ellipse
+   * @class
+   * @extends Two.Path
+   * @param {Number} [x=0] - The x position of the ellipse.
+   * @param {Number} [y=0] - The y position of the ellipse.
+   * @param {Number} rx - The radius value of the ellipse in the x direction.
+   * @param {Number} ry - The radius value of the ellipse in the y direction.
+   * @param {Number} [resolution=12] - The number of vertices used to construct the ellipse.
+   */
   var Ellipse = Two.Ellipse = function(ox, oy, rx, ry, resolution) {
 
     if (!_.isNumber(ry)) {
@@ -21,7 +31,15 @@
 
     Path.call(this, points, true, true, true);
 
+    /**
+     * @name Two.Ellipse#width
+     * @property {Number} - The width of the ellipse.
+     */
     this.width = rx * 2;
+    /**
+     * @name Two.Ellipse#height
+     * @property {Number} - The height of the ellipse.
+     */
     this.height = ry * 2;
 
     this._update();
@@ -31,8 +49,18 @@
 
   _.extend(Ellipse, {
 
+    /**
+     * @name Two.Ellipse.Properties
+     * @property {String[]} - A list of properties that are on every {@link Two.Ellipse}.
+     */
     Properties: ['width', 'height'],
 
+    /**
+     * @name Two.Ellipse.MakeObservable
+     * @function
+     * @param {Object} object - The object to make observable.
+     * @description Convenience function to apply observable qualities of a {@link Two.Ellipse} to any object. Handy if you'd like to extend the {@link Two.Ellipse} class on a custom class.
+     */
     MakeObservable: function(obj) {
 
       Path.MakeObservable(obj);
@@ -44,14 +72,42 @@
 
   _.extend(Ellipse.prototype, Path.prototype, {
 
-    _width: 0,
-    _height: 0,
-
+    /**
+     * @name Two.Ellipse#_flagWidth
+     * @private
+     * @property {Boolean} - Determines whether the {@link Two.Ellipse#width} needs updating.
+     */
     _flagWidth: false,
+    /**
+     * @name Two.Ellipse#_flagHeight
+     * @private
+     * @property {Boolean} - Determines whether the {@link Two.Ellipse#height} needs updating.
+     */
     _flagHeight: false,
+
+    /**
+     * @name Two.Circle#_width
+     * @private
+     * @see {@link Two.Ellipse#width}
+     */
+    _width: 0,
+    /**
+     * @name Two.Circle#_height
+     * @private
+     * @see {@link Two.Ellipse#height}
+     */
+    _height: 0,
 
     constructor: Ellipse,
 
+    /**
+     * @name Two.Ellipse#_update
+     * @function
+     * @private
+     * @param {Boolean} [bubbles=false] - Force the parent to `_update` as well.
+     * @description This is called before rendering happens by the renderer. This applies all changes necessary so that rendering is up-to-date but not updated more than it needs to be.
+     * @nota-bene Try not to call this method more than once a frame.
+     */
     _update: function() {
 
       if (this._flagWidth || this._flagHeight) {
@@ -89,6 +145,12 @@
 
     },
 
+    /**
+     * @name Two.Ellipse#flagReset
+     * @function
+     * @private
+     * @description Called internally to reset all flags. Ensures that only properties that change are updated before being sent to the renderer.
+     */
     flagReset: function() {
 
       this._flagWidth = this._flagHeight = false;
@@ -98,6 +160,13 @@
 
     },
 
+    /**
+     * @name Two.Ellipse#clone
+     * @function
+     * @param {Two.Group} [parent] - The parent group or scene to add the clone to.
+     * @returns {Two.Circle}
+     * @description Create a new instance of {@link Two.Circle} with the same properties of the current path.
+     */
     clone: function(parent) {
 
       var rx = this.width / 2;
