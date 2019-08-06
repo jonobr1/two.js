d7bd60c6aa280adbeb53056ced43d73750cad00e
Jono <developer@jono.fyi>
2019-08-06 18:43:10 +0200

    Rounded Rectangle Documentation

--- src/shapes/rounded-rectangle.js
+++ src/shapes/rounded-rectangle.js
@@ -3,6 +3,17 @@
   var Path = Two.Path;
   var _ = Two.Utils;
 
+  /**
+   * @name Two.RoundedRectangle
+   * @class
+   * @extends Two.Path
+   * @param {Number} [x=0] - The x position of the rounded rectangle.
+   * @param {Number} [y=0] - The y position of the rounded rectangle.
+   * @param {Number} width - The width value of the rounded rectangle.
+   * @param {Number} height - The width value of the rounded rectangle.
+   * @param {Number} radius - The radius value of the rounded rectangle.
+   * @param {Number} [resolution=12] - The number of vertices used to construct the rounded rectangle.
+   */
   var RoundedRectangle = Two.RoundedRectangle = function(ox, oy, width, height, radius) {
 
     if (_.isUndefined(radius)) {
@@ -25,8 +36,20 @@
 
     this._renderer.flagRadius = _.bind(RoundedRectangle.FlagRadius, this);
 
+    /**
+     * @name Two.RoundedRectangle#width
+     * @property {Number} - The width of the rounded rectangle.
+     */
     this.width = width;
+    /**
+     * @name Two.RoundedRectangle#height
+     * @property {Number} - The height of the rounded rectangle.
+     */
     this.height = height;
+    /**
+     * @name Two.RoundedRectangle#radius
+     * @property {Number} - The size of the radius of the rounded rectangle.
+     */
     this.radius = radius;
 
     this._update();
@@ -36,12 +59,26 @@
 
   _.extend(RoundedRectangle, {
 
+    /**
+     * @name Two.RoundedRectangle.Properties
+     * @property {String[]} - A list of properties that are on every {@link Two.RoundedRectangle}.
+     */
     Properties: ['width', 'height'],
 
+    /**
+     * @name Two.RoundedRectangle.FlagRadius
+     * @property {Function} - A convenience function to trigger the flag for radius changing.
+     */
     FlagRadius: function() {
       this._flagRadius = true;
     },
 
+    /**
+     * @name Two.RoundedRectangle.MakeObservable
+     * @function
+     * @param {Object} object - The object to make observable.
+     * @description Convenience function to apply observable qualities of a {@link Two.RoundedRectangle} to any object. Handy if you'd like to extend the {@link Two.RoundedRectangle} class on a custom class.
+     */
     MakeObservable: function(object) {
 
       Path.MakeObservable(object);
@@ -75,16 +112,54 @@
 
   _.extend(RoundedRectangle.prototype, Path.prototype, {
 
-    _width: 0,
-    _height: 0,
-    _radius: 0,
-
+    /**
+     * @name Two.RoundedRectangle#_flagWidth
+     * @private
+     * @property {Boolean} - Determines whether the {@link Two.RoundedRectangle#width} needs updating.
+     */
     _flagWidth: false,
+    /**
+     * @name Two.RoundedRectangle#_flagHeight
+     * @private
+     * @property {Boolean} - Determines whether the {@link Two.RoundedRectangle#height} needs updating.
+     */
     _flagHeight: false,
+    /**
+     * @name Two.RoundedRectangle#_flagRadius
+     * @private
+     * @property {Boolean} - Determines whether the {@link Two.RoundedRectangle#radius} needs updating.
+     */
     _flagRadius: false,
+
+    /**
+     * @name Two.RoundedRectangle#_width
+     * @private
+     * @see {@link Two.RoundedRectangle#width}
+     */
+    _width: 0,
+    /**
+     * @name Two.RoundedRectangle#_height
+     * @private
+     * @see {@link Two.RoundedRectangle#height}
+     */
+    _height: 0,
+    /**
+     * @name Two.RoundedRectangle#_radius
+     * @private
+     * @see {@link Two.RoundedRectangle#radius}
+     */
+    _radius: 0,
 
     constructor: RoundedRectangle,
 
+    /**
+     * @name Two.RoundedRectangle#_update
+     * @function
+     * @private
+     * @param {Boolean} [bubbles=false] - Force the parent to `_update` as well.
+     * @description This is called before rendering happens by the renderer. This applies all changes necessary so that rendering is up-to-date but not updated more than it needs to be.
+     * @nota-bene Try not to call this method more than once a frame.
+     */
     _update: function() {
 
       if (this._flagWidth || this._flagHeight || this._flagRadius) {
@@ -181,6 +256,12 @@
 
     },
 
+    /**
+     * @name Two.RoundedRectangle#flagReset
+     * @function
+     * @private
+     * @description Called internally to reset all flags. Ensures that only properties that change are updated before being sent to the renderer.
+     */
     flagReset: function() {
 
       this._flagWidth = this._flagHeight = this._flagRadius = false;
@@ -190,6 +271,13 @@
 
     },
 
+    /**
+     * @name Two.RoundedRectangle#clone
+     * @function
+     * @param {Two.Group} [parent] - The parent group or scene to add the clone to.
+     * @returns {Two.RoundedRectangle}
+     * @description Create a new instance of {@link Two.RoundedRectangle} with the same properties of the current path.
+     */
     clone: function(parent) {
 
       var width = this.width;
