6113d925ce693427c4d1c71d4fc597d544ad282b
Jono <developer@jono.fyi>
2019-08-06 18:26:57 +0200

    Polygon Documentation

--- src/shapes/polygon.js
+++ src/shapes/polygon.js
@@ -3,6 +3,15 @@
   var Path = Two.Path, TWO_PI = Math.PI * 2, cos = Math.cos, sin = Math.sin;
   var _ = Two.Utils;
 
+  /**
+   * @name Two.Polygon
+   * @class
+   * @extends Two.Path
+   * @param {Number} [x=0] - The x position of the polygon.
+   * @param {Number} [y=0] - The y position of the polygon.
+   * @param {Number} radius - The radius value of the polygon.
+   * @param {Number} [sides=12] - The number of vertices used to construct the polygon.
+   */
   var Polygon = Two.Polygon = function(ox, oy, r, sides) {
 
     sides = Math.max(sides || 0, 3);
@@ -12,8 +21,20 @@
     this.closed = true;
     this.automatic = false;
 
+    /**
+     * @name Two.Polygon#width
+     * @property {Number} - The size of the width of the polygon.
+     */
     this.width = r * 2;
+    /**
+     * @name Two.Polygon#height
+     * @property {Number} - The size of the height of the polygon.
+     */
     this.height = r * 2;
+    /**
+     * @name Two.Polygon#sides
+     * @property {Number} - The amount of sides the polyogn has.
+     */
     this.sides = sides;
 
     this._update();
@@ -23,8 +44,18 @@
 
   _.extend(Polygon, {
 
+    /**
+     * @name Two.Polygon.Properties
+     * @property {String[]} - A list of properties that are on every {@link Two.Polygon}.
+     */
     Properties: ['width', 'height', 'sides'],
 
+    /**
+     * @name Two.Polygon.MakeObservable
+     * @function
+     * @param {Object} object - The object to make observable.
+     * @description Convenience function to apply observable qualities of a {@link Two.Polygon} to any object. Handy if you'd like to extend the {@link Two.Polygon} class on a custom class.
+     */
     MakeObservable: function(obj) {
 
       Path.MakeObservable(obj);
@@ -36,16 +67,54 @@
 
   _.extend(Polygon.prototype, Path.prototype, {
 
-    _width: 0,
-    _height: 0,
-    _sides: 0,
-
+    /**
+     * @name Two.Polygon#_flagWidth
+     * @private
+     * @property {Boolean} - Determines whether the {@link Two.Polygon#width} needs updating.
+     */
     _flagWidth: false,
+    /**
+     * @name Two.Polygon#_flagHeight
+     * @private
+     * @property {Boolean} - Determines whether the {@link Two.Polygon#height} needs updating.
+     */
     _flagHeight: false,
+    /**
+     * @name Two.Polygon#_flagSides
+     * @private
+     * @property {Boolean} - Determines whether the {@link Two.Polygon#sides} needs updating.
+     */
     _flagSides: false,
+
+    /**
+     * @name Two.Polygon#_width
+     * @private
+     * @see {@link Two.Polygon#width}
+     */
+    _width: 0,
+    /**
+     * @name Two.Polygon#_height
+     * @private
+     * @see {@link Two.Polygon#height}
+     */
+    _height: 0,
+    /**
+     * @name Two.Polygon#_sides
+     * @private
+     * @see {@link Two.Polygon#sides}
+     */
+    _sides: 0,
 
     constructor: Polygon,
 
+    /**
+     * @name Two.Polygon#_update
+     * @function
+     * @private
+     * @param {Boolean} [bubbles=false] - Force the parent to `_update` as well.
+     * @description This is called before rendering happens by the renderer. This applies all changes necessary so that rendering is up-to-date but not updated more than it needs to be.
+     * @nota-bene Try not to call this method more than once a frame.
+     */
     _update: function() {
 
       if (this._flagWidth || this._flagHeight || this._flagSides) {
@@ -84,6 +153,12 @@
 
     },
 
+    /**
+     * @name Two.Polygon#flagReset
+     * @function
+     * @private
+     * @description Called internally to reset all flags. Ensures that only properties that change are updated before being sent to the renderer.
+     */
     flagReset: function() {
 
       this._flagWidth = this._flagHeight = this._flagSides = false;
@@ -93,6 +168,13 @@
 
     },
 
+    /**
+     * @name Two.Polygon#clone
+     * @function
+     * @param {Two.Group} [parent] - The parent group or scene to add the clone to.
+     * @returns {Two.Polygon}
+     * @description Create a new instance of {@link Two.Polygon} with the same properties of the current path.
+     */
     clone: function(parent) {
 
       var clone = new Polygon(0, 0, this.radius, this.sides);
