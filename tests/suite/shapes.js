954b9be5f77881c0517fd6fec2bc39f29deb9ad2
jonobr1 <developer@jono.fyi>
2021-08-19 20:24:48 -0400

    Added Core Getter / Setter Tests for Two.Points

--- tests/suite/shapes.js
+++ tests/suite/shapes.js
@@ -1,6 +1,56 @@
 
 QUnit.module('Primitives');
 
+QUnit.test('Two.Points', function(assert) {
+
+  var props = Two.Points.Properties.slice(0, 7);
+
+  assert.expect(props.length + 13);
+
+  var points = new Two.Points([
+    new Two.Vector(0, 0),
+    new Two.Vector(100, 100),
+    new Two.Vector(200, 200),
+    new Two.Vector(300, 300)
+  ]);
+
+  assert.equal(points.vertices.length, 4, 'Amount of vertices set correctly.');
+
+  points.fill = 'red';
+  assert.equal(points.fill, 'red', 'Can get property fill correctly.');
+  assert.equal(points._fill, 'red', 'Can set property fill correctly.');
+
+  points.stroke = 'red';
+  assert.equal(points.stroke, 'red', 'Can get property stroke correctly.');
+  assert.equal(points._stroke, 'red', 'Can set property stroke correctly.');
+
+  points.size = 6;
+  assert.equal(points.size, 6, 'Can get property size correctly.');
+  assert.equal(points._size, 6, 'Can set property size correctly.');
+
+  points.sizeAttenuation = true;
+  assert.equal(points.sizeAttenuation, true, 'Can get property sizeAttenuation correctly.');
+  assert.equal(points._sizeAttenuation, true, 'Can set property sizeAttenuation correctly.');
+
+  var dashes = [2, 2];
+  points.dashes = [2, 2];
+  assert.equal(points.dashes.length === 2 && points.dashes[0] === 2 && points.dashes[1] === 2, true, 'Can get property dashes correctly.');
+  assert.equal(points._dashes.length === 2 && points._dashes[0] === 2 && points._dashes[1] === 2, true, 'Can set property dashes correctly.');
+
+  points.className = 'ui-points';
+  assert.equal(points.className, 'ui-points', 'Can get property className correctly.');
+  assert.equal(points._className, 'ui-points', 'Can set property className correctly.');
+
+  points._update();
+  points.flagReset();
+
+  for (var i = 0; i < props.length; i++) {
+    var prop = props[i];
+    assert.equal(points['_flag' + prop.charAt(0).toUpperCase() + prop.slice(1)], false, 'Reset flag ' + prop + ' correctly.');
+  }
+
+});
+
 QUnit.test('Two.ArcSegment', function(assert) {
 
   assert.expect(Two.ArcSegment.Properties.length * 4 + 2);
@@ -15,7 +65,7 @@ QUnit.test('Two.ArcSegment', function(assert) {
   ];
 
   var path = new Two.ArcSegment(0, 0, innerRadius, outerRadius, startAngle, endAngle);
-  assert.equal(path.vertices.length, Two.Resolution * 3, 'amount of vertices set correctly.');
+  assert.equal(path.vertices.length, Two.Resolution * 3, 'Amount of vertices set correctly.');
 
   for (var i = 0; i < Two.ArcSegment.Properties.length; i++) {
     var prop = Two.ArcSegment.Properties[i];
@@ -48,7 +98,7 @@ QUnit.test('Two.Circle', function(assert) {
   var properties = [radius];
 
   var path = new Two.Circle(0, 0, radius);
-  assert.equal(path.vertices.length, 4, 'amount of vertices set correctly.');
+  assert.equal(path.vertices.length, 4, 'Amount of vertices set correctly.');
 
   for (var i = 0; i < Two.Circle.Properties.length; i++) {
     var prop = Two.Circle.Properties[i];
@@ -82,7 +132,7 @@ QUnit.test('Two.Ellipse', function(assert) {
   var properties = [rx * 2, ry * 2];
 
   var path = new Two.Ellipse(0, 0, rx, ry);
-  assert.equal(path.vertices.length, 4, 'amount of vertices set correctly.');
+  assert.equal(path.vertices.length, 4, 'Amount of vertices set correctly.');
 
   for (var i = 0; i < Two.Ellipse.Properties.length; i++) {
     var prop = Two.Ellipse.Properties[i];
