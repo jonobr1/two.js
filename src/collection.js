b2ac9f86e34fdbf7cd2ae82d08c9e3454a479241
Jono <developer@jono.fyi>
2021-10-07 08:58:57 -0700

    Converted Two.Collection to ES6

--- src/collection.js
+++ src/collection.js
@@ -7,52 +7,53 @@ import _ from './utils/underscore.js';
  * @extends Two.Events
  * @description An `Array` like object with additional event propagation on actions. `pop`, `shift`, and `splice` trigger `removed` events. `push`, `unshift`, and `splice` with more than 2 arguments trigger 'inserted'. Finally, `sort` and `reverse` trigger `order` events.
  */
-function Collection() {
 
-  Array.call(this);
+class Collection extends Array {
 
-  if (arguments[0] && Array.isArray(arguments[0])) {
-    if (arguments[0].length > 0) {
-      Array.prototype.push.apply(this, arguments[0]);
+  _events;
+  _bound = false;
+
+  constructor() {
+
+    super();
+    Events.call(this);
+
+    if (arguments[0] && Array.isArray(arguments[0])) {
+      if (arguments[0].length > 0) {
+        this.push.apply(this, arguments[0]);
+      }
+    } else if (arguments.length > 0) {
+      this.push.apply(this, arguments);
     }
-  } else if (arguments.length > 0) {
-    Array.prototype.push.apply(this, arguments);
+
   }
 
-}
-
-Collection.prototype = new Array();
-
-_.extend(Collection.prototype, Events, {
-
-  constructor: Collection,
-
-  pop: function() {
-    var popped = Array.prototype.pop.apply(this, arguments);
+  pop() {
+    var popped = super.pop.apply(this, arguments);
     this.trigger(Events.Types.remove, [popped]);
     return popped;
-  },
+  }
 
-  shift: function() {
-    var shifted = Array.prototype.shift.apply(this, arguments);
+  shift() {
+    var shifted = super.shift.apply(this, arguments);
     this.trigger(Events.Types.remove, [shifted]);
     return shifted;
   },
 
-  push: function() {
-    var pushed = Array.prototype.push.apply(this, arguments);
+  push() {
+    var pushed = super.push.apply(this, arguments);
     this.trigger(Events.Types.insert, arguments);
     return pushed;
   },
 
-  unshift: function() {
-    var unshifted = Array.prototype.unshift.apply(this, arguments);
+  unshift() {
+    var unshifted = super.unshift.apply(this, arguments);
     this.trigger(Events.Types.insert, arguments);
     return unshifted;
   },
 
-  splice: function() {
-    var spliced = Array.prototype.splice.apply(this, arguments);
+  splice() {
+    var spliced = super.splice.apply(this, arguments);
     var inserted;
 
     this.trigger(Events.Types.remove, spliced);
@@ -65,22 +66,24 @@ _.extend(Collection.prototype, Events, {
     return spliced;
   },
 
-  sort: function() {
-    Array.prototype.sort.apply(this, arguments);
+  sort() {
+    super.sort.apply(this, arguments);
     this.trigger(Events.Types.order);
     return this;
   },
 
-  reverse: function() {
-    Array.prototype.reverse.apply(this, arguments);
+  reverse() {
+    super.reverse.apply(this, arguments);
     this.trigger(Events.Types.order);
     return this;
   },
 
-  indexOf: function() {
-    return Array.prototype.indexOf.apply(this, arguments);
+  indexOf() {
+    return super.indexOf.apply(this, arguments);
   }
 
-});
+}
+
+Object.assign(Collection.prototype, Events.prototype);
 
 export default Collection;
