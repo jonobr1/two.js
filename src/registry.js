914fe19e9cdccbd90adf9b63ceb3aa5d224771aa
Jono <developer@jono.fyi>
2021-10-08 10:28:07 -0700

    Converted Two.Registry to ES6 Class

--- src/registry.js
+++ src/registry.js
@@ -5,15 +5,11 @@ import _ from './utils/underscore.js';
  * @class
  * @description An arbitrary class to manage a directory of things. Mainly used for keeping tabs of textures in Two.js.
  */
-function Registry() {
+class Registry {
 
-  this.map = {};
+  map = {};
 
-}
-
-_.extend(Registry.prototype, {
-
-  constructor: Registry,
+  constructor() {}
 
   /**
    * @name Two.Registry#add
@@ -22,10 +18,10 @@ _.extend(Registry.prototype, {
    * @param value - Any type of variable to be registered to the directory.
    * @description Adds any value to the directory. Assigned by the `id`.
    */
-  add: function(id, obj) {
+  add(id, obj) {
     this.map[id] = obj;
     return this;
-  },
+  }
 
   /**
    * @name Two.Registry#remove
@@ -33,10 +29,10 @@ _.extend(Registry.prototype, {
    * @param {String} id - A unique identifier.
    * @description Remove any value from the directory by its `id`.
    */
-  remove: function(id) {
+  remove(id) {
     delete this.map[id];
     return this;
-  },
+  }
 
   /**
    * @name Two.Registry#get
@@ -45,9 +41,9 @@ _.extend(Registry.prototype, {
    * @returns {?Object} The associated value. If unavailable then `undefined` is returned.
    * @description Get a registered value by its `id`.
    */
-  get: function(id) {
+  get(id) {
     return this.map[id];
-  },
+  }
 
   /**
    * @name Two.Registry#contains
@@ -56,10 +52,10 @@ _.extend(Registry.prototype, {
    * @returns {Boolean}
    * @description Convenience method to see if a value is registered to an `id` already.
    */
-  contains: function(id) {
+  contains(id) {
     return id in this.map;
   }
 
-});
+}
 
 export default Registry;
