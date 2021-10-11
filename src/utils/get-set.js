895ecfc7a0105e40787a8396c25971a801854448
Jono <developer@jono.fyi>
2021-10-10 19:01:44 -0700

    Converted get-set to ES6

--- src/utils/get-set.js
+++ src/utils/get-set.js
@@ -5,11 +5,11 @@
  * @param {String} property - The property to add an enumerable getter / setter to.
  * @description Convenience function to setup the flag based getter / setter that most properties are defined as in Two.js.
  */
-var defineGetterSetter = function(property) {
+export function defineGetterSetter(property) {
 
-  var object = this;
-  var secret = '_' + property;
-  var flag = '_flag' + property.charAt(0).toUpperCase() + property.slice(1);
+  const object = this;
+  const secret = '_' + property;
+  const flag = '_flag' + property.charAt(0).toUpperCase() + property.slice(1);
 
   Object.defineProperty(object, property, {
     enumerable: true,
@@ -22,6 +22,4 @@ var defineGetterSetter = function(property) {
     }
   });
 
-};
-
-export default defineGetterSetter;
+}
