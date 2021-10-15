8b424362bc55da92ee2f3daad4d7f68544b9afbb
Jono <developer@jono.fyi>
2021-10-15 15:49:23 -0700

    Converted Two.Circle to ES6 Class

--- src/shapes/circle.js
+++ src/shapes/circle.js
@@ -1,12 +1,11 @@
-import Commands from '../utils/path-commands.js';
+import { Commands } from '../utils/path-commands.js';
 import { HALF_PI, TWO_PI } from '../utils/math.js';
-import defineGetterSetter from '../utils/get-set.js';
-import _ from '../utils/underscore.js';
+import { _ } from '../utils/underscore.js';
 
-import Path from '../path.js';
-import Anchor from '../anchor.js';
+import { Path } from '../path.js';
+import { Anchor } from '../anchor.js';
 
-var cos = Math.cos, sin = Math.sin;
+const cos = Math.cos, sin = Math.sin;
 
 /**
  * @name Two.Circle
@@ -17,77 +16,61 @@ var cos = Math.cos, sin = Math.sin;
  * @param {Number} [radius=0] - The radius value of the circle.
  * @param {Number} [resolution=4] - The number of vertices used to construct the circle.
  */
-function Circle(ox, oy, r, resolution) {
-
-  // At least 2 vertices are required for proper circlage
-  var amount = resolution ? Math.max(resolution, 2) : 4;
-
-  var points = [];
-  for (var i = 0; i < amount; i++) {
-    points.push(new Anchor(0, 0, 0, 0, 0, 0));
-  }
-
-  Path.call(this, points, true, true, true);
-
-  /**
-   * @name Two.Circle#radius
-   * @property {Number} - The size of the radius of the circle.
-   */
-  if (typeof r === 'number') {
-    this.radius = r;
-  }
-
-  this._update();
-
-  if (typeof ox === 'number') {
-    this.translation.x = ox;
-  }
-  if (typeof oy === 'number') {
-    this.translation.y = oy;
-  }
-
-}
-
-_.extend(Circle, {
-
-  /**
-   * @name Two.Circle.Properties
-   * @property {String[]} - A list of properties that are on every {@link Two.Circle}.
-   */
-  Properties: ['radius'],
-
-  /**
-   * @name Two.Circle.MakeObservable
-   * @function
-   * @param {Object} object - The object to make observable.
-   * @description Convenience function to apply observable qualities of a {@link Two.Circle} to any object. Handy if you'd like to extend the {@link Two.Circle} class on a custom class.
-   */
-  MakeObservable: function(obj) {
-
-    Path.MakeObservable(obj);
-    _.each(Circle.Properties, defineGetterSetter, obj);
-
-  }
-
-});
-
-_.extend(Circle.prototype, Path.prototype, {
-
-  constructor: Circle,
+export class Circle extends Path {
 
   /**
    * @name Two.Circle#_flagRadius
    * @private
    * @property {Boolean} - Determines whether the {@link Two.Circle#radius} needs updating.
    */
-  _flagRadius: false,
+  _flagRadius = false;
 
   /**
    * @name Two.Circle#_radius
    * @private
    * @see {@link Two.Circle#radius}
    */
-  _radius: 0,
+  _radius = 0;
+
+  constructor(ox, oy, r, resolution) {
+
+    // At least 2 vertices are required for proper circlage
+    const amount = resolution ? Math.max(resolution, 2) : 4;
+    const points = [];
+    for (let i = 0; i < amount; i++) {
+      points.push(new Anchor(0, 0, 0, 0, 0, 0));
+    }
+
+    super(points, true, true, true);
+
+    for (let prop in proto) {
+      Object.defineProperty(this, prop, proto[prop]);
+    }
+
+    /**
+     * @name Two.Circle#radius
+     * @property {Number} - The size of the radius of the circle.
+     */
+    if (typeof r === 'number') {
+      this.radius = r;
+    }
+
+    this._update();
+
+    if (typeof ox === 'number') {
+      this.translation.x = ox;
+    }
+    if (typeof oy === 'number') {
+      this.translation.y = oy;
+    }
+
+  }
+
+  /**
+   * @name Two.Circle.Properties
+   * @property {String[]} - A list of properties that are on every {@link Two.Circle}.
+   */
+  static Properties = ['radius'];
 
   /**
    * @name Two.Circle#_update
@@ -97,35 +80,35 @@ _.extend(Circle.prototype, Path.prototype, {
    * @description This is called before rendering happens by the renderer. This applies all changes necessary so that rendering is up-to-date but not updated more than it needs to be.
    * @nota-bene Try not to call this method more than once a frame.
    */
-  _update: function() {
+  _update() {
 
     if (this._flagVertices || this._flagRadius) {
 
-      var length = this.vertices.length;
+      let length = this.vertices.length;
 
       if (!this._closed && length > 2) {
         length -= 1;
       }
 
       // Coefficient for approximating circular arcs with Bezier curves
-      var c = (4 / 3) * Math.tan(Math.PI / (length * 2));
-      var radius = this._radius;
-      var rc = radius * c;
+      const c = (4 / 3) * Math.tan(Math.PI / (length * 2));
+      const radius = this._radius;
+      const rc = radius * c;
 
-      for (var i = 0; i < this.vertices.length; i++) {
-        var pct = i / length;
-        var theta = pct * TWO_PI;
+      for (let i = 0; i < this.vertices.length; i++) {
+        const pct = i / length;
+        const theta = pct * TWO_PI;
 
-        var x = radius * cos(theta);
-        var y = radius * sin(theta);
+        const x = radius * cos(theta);
+        const y = radius * sin(theta);
 
-        var lx = rc * cos(theta - HALF_PI);
-        var ly = rc * sin(theta - HALF_PI);
+        const lx = rc * cos(theta - HALF_PI);
+        const ly = rc * sin(theta - HALF_PI);
 
-        var rx = rc * cos(theta + HALF_PI);
-        var ry = rc * sin(theta + HALF_PI);
+        const rx = rc * cos(theta + HALF_PI);
+        const ry = rc * sin(theta + HALF_PI);
 
-        var v = this.vertices[i];
+        const v = this.vertices[i];
 
         v.command = i === 0 ? Commands.move : Commands.curve;
         v.set(x, y);
@@ -134,10 +117,10 @@ _.extend(Circle.prototype, Path.prototype, {
       }
     }
 
-    Path.prototype._update.call(this);
+    super._update.call(this);
     return this;
 
-  },
+  }
 
   /**
    * @name Two.Circle#flagReset
@@ -145,14 +128,14 @@ _.extend(Circle.prototype, Path.prototype, {
    * @private
    * @description Called internally to reset all flags. Ensures that only properties that change are updated before being sent to the renderer.
    */
-  flagReset: function() {
+  flagReset() {
 
     this._flagRadius = false;
 
-    Path.prototype.flagReset.call(this);
+    super.flagReset.call(this);
     return this;
 
-  },
+  }
 
   /**
    * @name Two.Circle#clone
@@ -161,9 +144,9 @@ _.extend(Circle.prototype, Path.prototype, {
    * @returns {Two.Circle}
    * @description Create a new instance of {@link Two.Circle} with the same properties of the current path.
    */
-  clone: function(parent) {
+  clone(parent) {
 
-    var clone = new Circle(0, 0, this.radius, this.vertices.length);
+    const clone = new Circle(0, 0, this.radius, this.vertices.length);
 
     clone.translation.copy(this.translation);
     clone.rotation = this.rotation;
@@ -185,7 +168,7 @@ _.extend(Circle.prototype, Path.prototype, {
 
     return clone;
 
-  },
+  }
 
   /**
    * @name Two.Circle#toObject
@@ -193,9 +176,9 @@ _.extend(Circle.prototype, Path.prototype, {
    * @returns {Object}
    * @description Return a JSON compatible plain object that represents the path.
    */
-  toObject: function() {
+  toObject() {
 
-    var object = Path.prototype.toObject.call(this);
+    const object = super.toObject.call(this);
 
     _.each(Circle.Properties, function(property) {
       object[property] = this[property];
@@ -205,8 +188,17 @@ _.extend(Circle.prototype, Path.prototype, {
 
   }
 
-});
+}
 
-Circle.MakeObservable(Circle.prototype);
-
-export default Circle;
+const proto = {
+  radius: {
+    enumerable: true,
+    get: function() {
+      return this._radius;
+    },
+    set: function(v) {
+      this._radius = v;
+      this._flagRadius = true;
+    }
+  }
+};
