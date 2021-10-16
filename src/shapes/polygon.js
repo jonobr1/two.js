d583b404ef6183a375457d10210ca6b10c167d29
Jono <developer@jono.fyi>
2021-10-16 09:46:27 -0700

    Converted Two.Polygon to ES6 Class

--- src/shapes/polygon.js
+++ src/shapes/polygon.js
@@ -1,12 +1,11 @@
-import Commands from '../utils/path-commands.js';
+import { Commands } from '../utils/path-commands.js';
 import { TWO_PI } from '../utils/math.js';
-import defineGetterSetter from '../utils/get-set.js';
-import _ from '../utils/underscore.js';
+import { _ } from '../utils/underscore.js';
 
-import Path from '../path.js';
-import Anchor from '../anchor.js';
+import { Path } from '../path.js';
+import { Anchor } from '../anchor.js';
 
-var cos = Math.cos, sin = Math.sin;
+const cos = Math.cos, sin = Math.sin;
 
 /**
  * @name Two.Polygon
@@ -17,114 +16,99 @@ var cos = Math.cos, sin = Math.sin;
  * @param {Number} [radius=0] - The radius value of the polygon.
  * @param {Number} [sides=12] - The number of vertices used to construct the polygon.
  */
-function Polygon(ox, oy, r, sides) {
-
-  sides = Math.max(sides || 0, 3);
-
-  Path.call(this);
-
-  this.closed = true;
-  this.automatic = false;
-
-  /**
-   * @name Two.Polygon#width
-   * @property {Number} - The size of the width of the polygon.
-   */
-  if (typeof r === 'number') {
-    this.width = r * 2;
-  }
-
-  /**
-   * @name Two.Polygon#height
-   * @property {Number} - The size of the height of the polygon.
-   */
-  if (typeof r === 'number') {
-    this.height = r * 2;
-  }
-
-  /**
-   * @name Two.Polygon#sides
-   * @property {Number} - The amount of sides the polyogn has.
-   */
-  if (typeof sides === 'number') {
-    this.sides = sides;
-  }
-
-  this._update();
-
-  if (typeof ox === 'number') {
-    this.translation.x = ox;
-  }
-  if (typeof oy === 'number') {
-    this.translation.y = oy;
-  }
-
-}
-
-_.extend(Polygon, {
-
-  /**
-   * @name Two.Polygon.Properties
-   * @property {String[]} - A list of properties that are on every {@link Two.Polygon}.
-   */
-  Properties: ['width', 'height', 'sides'],
-
-  /**
-   * @name Two.Polygon.MakeObservable
-   * @function
-   * @param {Object} object - The object to make observable.
-   * @description Convenience function to apply observable qualities of a {@link Two.Polygon} to any object. Handy if you'd like to extend the {@link Two.Polygon} class on a custom class.
-   */
-  MakeObservable: function(obj) {
-
-    Path.MakeObservable(obj);
-    _.each(Polygon.Properties, defineGetterSetter, obj);
-
-  }
-
-});
-
-_.extend(Polygon.prototype, Path.prototype, {
-
-  constructor: Polygon,
+export class Polygon extends Path {
 
   /**
    * @name Two.Polygon#_flagWidth
    * @private
    * @property {Boolean} - Determines whether the {@link Two.Polygon#width} needs updating.
    */
-  _flagWidth: false,
+  _flagWidth = false;
   /**
    * @name Two.Polygon#_flagHeight
    * @private
    * @property {Boolean} - Determines whether the {@link Two.Polygon#height} needs updating.
    */
-  _flagHeight: false,
+  _flagHeight = false;
   /**
    * @name Two.Polygon#_flagSides
    * @private
    * @property {Boolean} - Determines whether the {@link Two.Polygon#sides} needs updating.
    */
-  _flagSides: false,
+  _flagSides = false;
 
   /**
    * @name Two.Polygon#_width
    * @private
    * @see {@link Two.Polygon#width}
    */
-  _width: 0,
+  _width = 0;
   /**
    * @name Two.Polygon#_height
    * @private
    * @see {@link Two.Polygon#height}
    */
-  _height: 0,
+  _height = 0;
   /**
    * @name Two.Polygon#_sides
    * @private
    * @see {@link Two.Polygon#sides}
    */
-  _sides: 0,
+  _sides = 0;
+
+  constructor(ox, oy, r, sides) {
+
+    sides = Math.max(sides || 0, 3);
+
+    super();
+
+    for (let prop in proto) {
+      Object.defineProperty(this, prop, proto[prop]);
+    }
+
+    this.closed = true;
+    this.automatic = false;
+
+    /**
+     * @name Two.Polygon#width
+     * @property {Number} - The size of the width of the polygon.
+     */
+    if (typeof r === 'number') {
+      this.width = r * 2;
+    }
+
+    /**
+     * @name Two.Polygon#height
+     * @property {Number} - The size of the height of the polygon.
+     */
+    if (typeof r === 'number') {
+      this.height = r * 2;
+    }
+
+    /**
+     * @name Two.Polygon#sides
+     * @property {Number} - The amount of sides the polyogn has.
+     */
+    if (typeof sides === 'number') {
+      this.sides = sides;
+    }
+
+    this._update();
+
+    if (typeof ox === 'number') {
+      this.translation.x = ox;
+    }
+    if (typeof oy === 'number') {
+      this.translation.y = oy;
+    }
+
+  }
+
+  /**
+   * @name Two.Polygon.Properties
+   * @property {String[]} - A list of properties that are on every {@link Two.Polygon}.
+   */
+  static Properties = ['width', 'height', 'sides'];
 
   /**
    * @name Two.Polygon#_update
@@ -134,25 +118,25 @@ _.extend(Polygon.prototype, Path.prototype, {
    * @description This is called before rendering happens by the renderer. This applies all changes necessary so that rendering is up-to-date but not updated more than it needs to be.
    * @nota-bene Try not to call this method more than once a frame.
    */
-  _update: function() {
+  _update() {
 
     if (this._flagVertices || this._flagWidth || this._flagHeight || this._flagSides) {
 
-      var sides = this._sides;
-      var amount = sides + 1;
-      var length = this.vertices.length;
+      const sides = this._sides;
+      const amount = sides + 1;
+      let length = this.vertices.length;
 
       if (length > sides) {
         this.vertices.splice(sides - 1, length - sides);
         length = sides;
       }
 
-      for (var i = 0; i < amount; i++) {
+      for (let i = 0; i < amount; i++) {
 
-        var pct = (i + 0.5) / sides;
-        var theta = TWO_PI * pct + Math.PI / 2;
-        var x = this._width * cos(theta) / 2;
-        var y = this._height * sin(theta) / 2;
+        const pct = (i + 0.5) / sides;
+        const theta = TWO_PI * pct + Math.PI / 2;
+        const x = this._width * cos(theta) / 2;
+        const y = this._height * sin(theta) / 2;
 
         if (i >= length) {
           this.vertices.push(new Anchor(x, y));
@@ -166,10 +150,10 @@ _.extend(Polygon.prototype, Path.prototype, {
 
     }
 
-    Path.prototype._update.call(this);
+    super._update.call(this);
     return this;
 
-  },
+  }
 
   /**
    * @name Two.Polygon#flagReset
@@ -177,14 +161,14 @@ _.extend(Polygon.prototype, Path.prototype, {
    * @private
    * @description Called internally to reset all flags. Ensures that only properties that change are updated before being sent to the renderer.
    */
-  flagReset: function() {
+  flagReset() {
 
     this._flagWidth = this._flagHeight = this._flagSides = false;
-    Path.prototype.flagReset.call(this);
+    super.flagReset.call(this);
 
     return this;
 
-  },
+  }
 
   /**
    * @name Two.Polygon#clone
@@ -193,9 +177,9 @@ _.extend(Polygon.prototype, Path.prototype, {
    * @returns {Two.Polygon}
    * @description Create a new instance of {@link Two.Polygon} with the same properties of the current path.
    */
-  clone: function(parent) {
+  clone(parent) {
 
-    var clone = new Polygon(0, 0, this.radius, this.sides);
+    const clone = new Polygon(0, 0, this.radius, this.sides);
 
     clone.translation.copy(this.translation);
     clone.rotation = this.rotation;
@@ -217,7 +201,7 @@ _.extend(Polygon.prototype, Path.prototype, {
 
     return clone;
 
-  },
+  }
 
   /**
    * @name Two.Polygon#toObject
@@ -225,9 +209,9 @@ _.extend(Polygon.prototype, Path.prototype, {
    * @returns {Object}
    * @description Return a JSON compatible plain object that represents the path.
    */
-  toObject: function() {
+  toObject() {
 
-    var object = Path.prototype.toObject.call(this);
+    const object = super.toObject.call(this);
 
     _.each(Polygon.Properties, function(property) {
       object[property] = this[property];
@@ -237,8 +221,37 @@ _.extend(Polygon.prototype, Path.prototype, {
 
   }
 
-});
+}
 
-Polygon.MakeObservable(Polygon.prototype);
-
-export default Polygon;
+const proto = {
+  width: {
+    enumerable: true,
+    get: function() {
+      return this._width;
+    },
+    set: function(v) {
+      this._width = v;
+      this._flagWidth = true;
+    }
+  },
+  height: {
+    enumerable: true,
+    get: function() {
+      return this._height;
+    },
+    set: function(v) {
+      this._height = v;
+      this._flagHeight = true;
+    }
+  },
+  sides: {
+    enumerable: true,
+    get: function() {
+      return this._sides;
+    },
+    set: function(v) {
+      this._sides = v;
+      this._flagSides = true;
+    }
+  }
+};
