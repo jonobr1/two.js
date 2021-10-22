e579f74602a3911ac1202a83749b6512aa330cd5
Jono <developer@jono.fyi>
2021-10-21 20:28:50 -0700

    Converted Two.RadialGradient to ES6 Class

--- src/effects/radial-gradient.js
+++ src/effects/radial-gradient.js
@@ -1,10 +1,9 @@
-import Events from '../events.js';
-import defineGetterSetter from '../utils/get-set.js';
-import _ from '../utils/underscore.js';
+import { Events } from '../events.js';
+import { _ } from '../utils/underscore.js';
 
-import Stop from './stop.js';
-import Gradient from './gradient.js';
-import Vector from '../vector.js';
+import { Stop } from './stop.js';
+import { Gradient } from './gradient.js';
+import { Vector } from '../vector.js';
 
 /**
  * @name Two.RadialGradient
@@ -18,105 +17,87 @@ import Vector from '../vector.js';
  * @param {Number} [focalY=0] - The y position of the focal point on the radial gradient.
  * @nota-bene The radial gradient lives within the space of the parent object's matrix space.
  */
-function RadialGradient(cx, cy, r, stops, fx, fy) {
-
-  Gradient.call(this, stops);
-
-  this._renderer.type = 'radial-gradient';
-
-  /**
-   * @name Two.RadialGradient#center
-   * @property {Two.Vector} - The x and y value for where the origin of the radial gradient is.
-   */
-  this.center = new Vector()
-    .bind(Events.Types.change, (function() {
-      this._flagCenter = true;
-    }).bind(this));
-
-  this.radius = typeof r === 'number' ? r : 20;
-
-  /**
-   * @name Two.RadialGradient#focal
-   * @property {Two.Vector} - The x and y value for where the focal point of the radial gradient is.
-   * @nota-bene This effects the spray or spread of the radial gradient.
-   */
-  this.focal = new Vector()
-    .bind(Events.Types.change, (function() {
-      this._flagFocal = true;
-    }).bind(this));
-
-  if (typeof cx === 'number') {
-    this.center.x = cx;
-  }
-  if (typeof cy === 'number') {
-    this.center.y = cy;
-  }
-
-  this.focal.copy(this.center);
-
-  if (typeof fx === 'number') {
-    this.focal.x = fx;
-  }
-  if (typeof fy === 'number') {
-    this.focal.y = fy;
-  }
-
-}
-
-_.extend(RadialGradient, {
-
-  /**
-   * @name Two.RadialGradient.Stop
-   * @see {@link Two.Stop}
-   */
-  Stop: Stop,
-
-  /**
-   * @name Two.RadialGradient.Properties
-   * @property {String[]} - A list of properties that are on every {@link Two.RadialGradient}.
-   */
-  Properties: [
-    'radius'
-  ],
-
-  /**
-   * @name Two.RadialGradient.MakeObservable
-   * @function
-   * @param {Object} object - The object to make observable.
-   * @description Convenience function to apply observable qualities of a {@link Two.RadialGradient} to any object. Handy if you'd like to extend the {@link Two.RadialGradient} class on a custom class.
-   */
-  MakeObservable: function(object) {
-
-    Gradient.MakeObservable(object);
-
-    _.each(RadialGradient.Properties, defineGetterSetter, object);
-
-  }
-
-});
-
-_.extend(RadialGradient.prototype, Gradient.prototype, {
-
-  constructor: RadialGradient,
+export class RadialGradient extends Gradient {
 
   /**
    * @name Two.RadialGradient#_flagRadius
    * @private
    * @property {Boolean} - Determines whether the {@link Two.RadialGradient#radius} changed and needs to update.
    */
-  _flagRadius: false,
+  _flagRadius = false;
   /**
    * @name Two.RadialGradient#_flagCenter
    * @private
    * @property {Boolean} - Determines whether the {@link Two.RadialGradient#center} changed and needs to update.
    */
-  _flagCenter: false,
+  _flagCenter = false;
   /**
    * @name Two.RadialGradient#_flagFocal
    * @private
    * @property {Boolean} - Determines whether the {@link Two.RadialGradient#focal} changed and needs to update.
    */
-  _flagFocal: false,
+  _flagFocal = false;
+
+  _radius = 0;
+  _center = null
+  _focal = null;
+
+  constructor(cx, cy, r, stops, fx, fy) {
+
+    super(stops);
+
+    for (let prop in proto) {
+      Object.defineProperty(this, prop, proto[prop]);
+    }
+
+    this._renderer.type = 'radial-gradient';
+    this._renderer.flagCenter = FlagCenter.bind(this);
+    this._renderer.flagFocal = FlagFocal.bind(this);
+
+    /**
+     * @name Two.RadialGradient#center
+     * @property {Two.Vector} - The x and y value for where the origin of the radial gradient is.
+     */
+    this.center = new Vector();
+
+    this.radius = typeof r === 'number' ? r : 20;
+
+    /**
+     * @name Two.RadialGradient#focal
+     * @property {Two.Vector} - The x and y value for where the focal point of the radial gradient is.
+     * @nota-bene This effects the spray or spread of the radial gradient.
+     */
+    this.focal = new Vector();
+
+    if (typeof cx === 'number') {
+      this.center.x = cx;
+    }
+    if (typeof cy === 'number') {
+      this.center.y = cy;
+    }
+
+    this.focal.copy(this.center);
+
+    if (typeof fx === 'number') {
+      this.focal.x = fx;
+    }
+    if (typeof fy === 'number') {
+      this.focal.y = fy;
+    }
+
+  }
+
+  /**
+   * @name Two.RadialGradient.Stop
+   * @see {@link Two.Stop}
+   */
+  static Stop = Stop;
+
+  /**
+   * @name Two.RadialGradient.Properties
+   * @property {String[]} - A list of properties that are on every {@link Two.RadialGradient}.
+   */
+  static Properties = ['center', 'radius', 'focal'];
 
   /**
    * @name Two.RadialGradient#clone
@@ -125,13 +106,13 @@ _.extend(RadialGradient.prototype, Gradient.prototype, {
    * @returns {Two.Gradient}
    * @description Create a new instance of {@link Two.RadialGradient} with the same properties of the current path.
    */
-  clone: function(parent) {
+  clone(parent) {
 
-    var stops = this.stops.map(function(stop) {
+    const stops = this.stops.map(function(stop) {
       return stop.clone();
     });
 
-    var clone = new RadialGradient(this.center._x, this.center._y,
+    const clone = new RadialGradient(this.center._x, this.center._y,
         this._radius, stops, this.focal._x, this.focal._y);
 
     _.each(Gradient.Properties.concat(RadialGradient.Properties), function(k) {
@@ -144,7 +125,7 @@ _.extend(RadialGradient.prototype, Gradient.prototype, {
 
     return clone;
 
-  },
+  }
 
   /**
    * @name Two.RadialGradient#toObject
@@ -152,9 +133,9 @@ _.extend(RadialGradient.prototype, Gradient.prototype, {
    * @returns {Object}
    * @description Return a JSON compatible plain object that represents the path.
    */
-  toObject: function() {
+  toObject() {
 
-    var result = Gradient.prototype.toObject.call(this);
+    const result = super.toObject.call(this);
 
     _.each(RadialGradient.Properties, function(k) {
       result[k] = this[k];
@@ -165,7 +146,7 @@ _.extend(RadialGradient.prototype, Gradient.prototype, {
 
     return result;
 
-  },
+  }
 
   /**
    * @name Two.RadialGradient#_update
@@ -175,7 +156,7 @@ _.extend(RadialGradient.prototype, Gradient.prototype, {
    * @description This is called before rendering happens by the renderer. This applies all changes necessary so that rendering is up-to-date but not updated more than it needs to be.
    * @nota-bene Try not to call this method more than once a frame.
    */
-  _update: function() {
+  _update() {
 
     if (this._flagRadius || this._flatCenter || this._flagFocal
       || this._flagSpread || this._flagStops) {
@@ -184,7 +165,7 @@ _.extend(RadialGradient.prototype, Gradient.prototype, {
 
     return this;
 
-  },
+  }
 
   /**
    * @name Two.RadialGradient#flagReset
@@ -192,18 +173,63 @@ _.extend(RadialGradient.prototype, Gradient.prototype, {
    * @private
    * @description Called internally to reset all flags. Ensures that only properties that change are updated before being sent to the renderer.
    */
-  flagReset: function() {
+  flagReset() {
 
     this._flagRadius = this._flagCenter = this._flagFocal = false;
 
-    Gradient.prototype.flagReset.call(this);
+    super.flagReset.call(this);
 
     return this;
 
   }
 
-});
+}
 
-RadialGradient.MakeObservable(RadialGradient.prototype);
+const proto = {
+  radius: {
+    enumerable: true,
+    get: function() {
+      return this._radius;
+    },
+    set: function(v) {
+      this._radius = v;
+      this._flagRadius = true;
+    }
+  },
+  center: {
+    enumerable: true,
+    get: function() {
+      return this._center;
+    },
+    set: function(v) {
+      if (this._center) {
+        this._center.unbind(Events.Types.change, this._renderer.flagCenter);
+      }
+      this._center = v;
+      this._center.bind(Events.Types.change, this._renderer.flagCenter);
+      this._flagCenter = true;
+    }
+  },
+  focal: {
+    enumerable: true,
+    get: function() {
+      return this._focal;
+    },
+    set: function(v) {
+      if (this._focal) {
+        this._focal.unbind(Events.Types.change, this._renderer.flagFocal);
+      }
+      this._focal = v;
+      this._focal.bind(Events.Types.change, this._renderer.flagFocal);
+      this._flagFocal = true;
+    }
+  }
+};
 
-export default RadialGradient;
+function FlagCenter() {
+  this._flagCenter = true;
+}
+
+function FlagFocal() {
+  this._flagFocal = true;
+}
