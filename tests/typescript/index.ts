b678a11555b6dbd0711b7030173cc3a896923a7c
Jono Brandel <developer@jono.fyi>
2025-09-16 12:53:44 -0700

    Improved TypeScript tests

--- tests/typescript/index.ts
+++ tests/typescript/index.ts
@@ -1,8 +1,8 @@
-import Two from "two.js";
+import Two from 'two.js';
 
 const two = new Two({
   fullscreen: true,
-  autostart: false
+  autostart: false,
 });
 
 let path;
@@ -10,40 +10,36 @@ let path;
 path = new Two.Line(5, 5, 10, 10);
 two.add(path);
 
+const group = new Two.Group(path);
+two.add(group);
+
 path = new Two.Circle(3, 3, 10);
-two.add(path);
+group.add(path);
 
 path = new Two.Rectangle(25, 25, 5, 5);
-two.add(path);
+group.add(path);
 
 path = new Two.Polygon(30, 30, 5, 7);
-two.add(path);
+group.add(path);
 
 path = new Two.ArcSegment(5, 25, 5, 10, 0, Math.PI);
-two.add(path);
+group.add(path);
 
 path = new Two.Ellipse(5, 30, 10, 5);
-two.add(path);
+group.add(path);
 
 path = new Two.Star(30, 5, 10, 5);
-two.add(path);
+group.add(path);
 
-path = new Two.Points([
-  new Two.Anchor(-2, 0),
-  new Two.Anchor(2, 0)
-]);
-two.add(path);
+path = new Two.Points([new Two.Anchor(-2, 0), new Two.Anchor(2, 0)]);
+group.add(path);
 
-path = new Two.Path([
-  new Two.Anchor(-1, 0),
-  new Two.Anchor(1, 0)
-]);
+path = new Two.Path([new Two.Anchor(-1, 0), new Two.Anchor(1, 0)]);
 path.closed = false;
 path.automatic = true;
-path.position.set(
-  two.width / 2, two.height / 2
-);
+
+path.position.set(two.width / 2, two.height / 2);
 path.scale = new Two.Vector(1, 1);
-two.add(path);
+group.add(path);
 
 two.appendTo(document.body);
